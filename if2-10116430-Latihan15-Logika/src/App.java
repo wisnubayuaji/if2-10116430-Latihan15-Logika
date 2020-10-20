d9d2ff7adf850027f0c6cc7167963e89b2a517c1
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:46:54 +0700

    Add files via upload

---
+++ if2-10116430-Latihan15-Logika/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
